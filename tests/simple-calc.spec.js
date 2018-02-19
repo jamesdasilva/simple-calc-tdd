42911ba004a60a72581f80a3440dad2c95ae43bb
James da Silva <jamesodas@gmail.com>
2018-02-19 19:08:58 -0300

    O método div foi criado

--- tests/simple-calc.spec.js
+++ tests/simple-calc.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { sum, sub, mult } from '../src/simple-calc.js';
+import { sum, sub, mult, div } from '../src/simple-calc.js';
 
 describe('SimpleCalc', () => {
 
@@ -19,6 +19,11 @@ describe('SimpleCalc', () => {
       it('shoult exist the method `mult`', () => {
         expect(mult).to.exist;
         expect(mult).to.be.a('function');
+      });
+
+      it('shoult exist the method `div`', () => {
+        expect(div).to.exist;
+        expect(div).to.be.a('function');
       });
 
   });
@@ -47,6 +52,15 @@ describe('SimpleCalc', () => {
     });
     it('should return 6 when `mult(2, -4)`', () => {
       expect(mult(2, -4)).to.be.equal(-8);
+    });
+  });
+
+  describe('Div', () => {
+    it('should return 4 when `div(8, 2)`', () => {
+      expect(div(8, 2)).to.be.equal(4);
+    });
+    it('should return `Não é possível a divisão por 0` when divide by 0', () => {
+      expect(div(8, 0)).to.be.equal('Não é possível a divisão por 0');
     });
   });
 
